ac8eec1aef8a4a938198ab99155f8eb7d32aa0f5
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-17 07:29:14 -0500

    Completed post route for Tag.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -17,9 +17,16 @@ router.get('/:id', (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
