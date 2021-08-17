e916ab7a5d596f8f6d437fcb7a913ece772fb05d
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-17 07:43:16 -0500

    Completed delete route for category-route...Completed all api routes for category-route.js

--- Develop/routes/api/category-route.js
+++ Develop/routes/api/category-route.js
@@ -55,7 +55,7 @@ router.put('/:id', async (req, res) => {
                 id: req.params.id,
             }
         });
-        res.status(200).json(tagData);
+        res.status(200).json(categoryData);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -64,6 +64,16 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', (req, res) => {
     // delete a category by its `id` value
+    try {
+        const categoryData = await Category.destroy({
+            where: {
+                id: req.params.id,
+            }
+        });
+        res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
